68bef53f2d54457389645e5244b7619fe16aac49
San Lee <sanlee2017@u.northwestern.edu>
2018-04-14 22:41:52 -0500

    new design and html structure for survey

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { SurveyResultService } from '../service/survery-result.service';
 import { ResponseForm } from '../model/response-form';
@@ -10,35 +10,49 @@ import { Router } from '@angular/router';
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.css']
 })
-export class SurveyComponent implements OnInit {
-  res_one: number;
-  res_two: number;
-  res_three: number;
+export class SurveyComponent implements OnInit, AfterViewInit {
+  res_one = -1;
+  res_two = -1;
+  res_three = -1;
   res = [];
   slideIndex = 1;
 
   // mock quizzes
   // questions
-  questions = ['Which sensory experience would be the most appealing to you?',
-              'When and where might you wear the fragrance you’re looking for today?',
-              'Which of the following scent types appeals most to you?'];
+  questions = ['How would you want to spend your vacation?',
+              'Who are you at a party?',
+              'How do you want others to remember you?'];
 
   option_one = ['Enjoying a fruity drink and an ocean breeze', 'Walking through a serene garden in full bloom',
     'Tasting exotic cuisines and decadent desserts', 'Adventuring off the grid and deep into nature'];
-  option_two = ['work or school', 'formal event', 'casual outing', 'kickback'];
-  option_three = ['aquatic and fruity', 'floral', 'spicy', 'woody'];
+  option_two = ['Fly on the wall', 'The life of the party', 'Deep converser', 'Nah, I\'d stay at home'];
+  option_three = ['Nice and Friendly', 'Intelligent and wise', 'Achiever', 'Mediator'];
+  options = [this.option_one, this.option_two, this.option_three];
 
   constructor(public router: Router,
               public surResult: SurveyResultService,
               private elementRef: ElementRef) { }
 
-  register() {
+  register(i) {
+    console.log(this.slideIndex);
+    if (this.res[this.slideIndex - 1] !== undefined) {
+      console.log('not null');
+      console.log(this.res[this.slideIndex - 1]);
+      this.res[this.slideIndex - 1] = i;
+    } else {
+      this.res.push(i);
+    }
+
+    console.log(this.res);
+    if (this.res.length >= this.questions.length) {
+      this.submit(this.res);
+    }
+    this.plusSlides(1);
   }
 
   showSlides(n?) {
     let i;
     const slides = document.getElementsByClassName('slides');
-    console.log(slides);
     const dots = document.getElementsByClassName('dot');
     if (n > slides.length) {
       this.slideIndex = 1;
@@ -52,28 +66,34 @@ export class SurveyComponent implements OnInit {
       (slides[i] as HTMLElement).style.display = 'none';
     }
 
-
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace('active', '');
     }
     (slides[this.slideIndex - 1] as HTMLElement).style.display = 'block';
-    console.log(dots[this.slideIndex - 1]);
     (dots[this.slideIndex - 1] as HTMLElement).classList.add('active');
   }
 
   // Next/previous controls
   plusSlides(n) {
-    console.log(n);
+    console.log(this.res_one);
     this.showSlides(this.slideIndex += n);
   }
 
-  submit(res_one, res_two) {
-    this.surResult.get_card(this.res_one, this.res_two);
+  // API call for card
+  submit(res) {
+    this.surResult.get_card(this.res[0], this.res[1]);
     this.router.navigate(['card']);
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.showSlides();
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  ngOnInit() {
   }
 
 }
