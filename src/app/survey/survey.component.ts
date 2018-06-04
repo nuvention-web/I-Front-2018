e4dbfbd0b585f427c7e2e5681314d1eabed419cb
San Lee <sanlee2017@u.northwestern.edu>
2018-06-03 22:46:45 -0500

    removed arrows and fixed typos

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -22,17 +22,16 @@ export class SurveyComponent implements OnInit, AfterViewInit, AfterContentCheck
   questions = ['How would you want to spend your vacation?',
               'Who are you at a party?',
               'How do you want others to remember you?',
-              'If a time machine was just released,  would you go?',
+              'If a time machine was just released, where would you go?',
               'What is your ideal night out?',
               'What is your style?'];
 
   option_one = ['Enjoying a fruity drink and an ocean breeze', 'Walking through a serene garden in full bloom',
     'Tasting exotic cuisines and decadent desserts', 'Adventuring off the grid and deep into nature'];
   option_two = ['Fly on the wall', 'The life of the party', 'Deep converser', 'Nah, I\'d stay at home'];
-  option_three = ['Nice and fiendly', 'Intelligent and wise', 'Candid and open', 'Warm and caring'];
+  option_three = ['Nice and friendly', 'Intelligent and wise', 'Candid and open', 'Warm and caring'];
   option_four = ['90s', '2000s', '2010s', 'Just stay where I am'];
   option_five = ['Clubbing', 'Kickback', 'Night in', 'Dinner and movie'];
-  // option_six = ['Work or school', 'Formal event', 'Casual outing', 'Chillin\''];
   option_seven = ['Simple and classic', 'Trendy and hipster', 'Casual and comfortable', 'Dress to impress'];
   options = [this.option_one, this.option_two, this.option_three, this.option_four, this.option_five, this.option_seven];
 
